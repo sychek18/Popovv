6cd1b77bc15aa8764aef7f63c02280fc2324d657
Maksym Sych <maks.sychek7@gmail.com>
2020-09-03 14:36:19 +0300

    пвчп

---
+++ test2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Popovv
+{
+    /// <summary>
+    /// Логика взаимодействия для test2.xaml
+    /// </summary>
+    public partial class test2 : Window
+    {
+        public test2()
+        {
+            InitializeComponent();
+        }
+    }
+}
